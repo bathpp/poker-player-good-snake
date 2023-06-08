26f20436bfb5c1f024f0741e82a12054e9e6e9bc
yulin he <yulin.he@rushstreetinteractive.com>
2023-06-08 11:21:25 -0400

    v5

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.4';
+    return '0.5';
   }
 
   checkSuit(cards) {
@@ -17,23 +17,23 @@ class Player {
     return suitCount;
   }
 
-  valueConvert(value) {
+  rankConvert(rank) {
     const faces = ['J', 'Q', 'K', 'A'];
-    if (faces.includes(value)) {
-      return 11 + faces.indexOf(value)
+    if (faces.includes(rank)) {
+      return 11 + faces.indexOf(rank)
     }
-    return Number(value);
+    return Number(rank);
   }
 
   static twoCardCheck(cards) {
     const [card1, card2] = cards;
     // const { rank: card1Rank, value: card1Suit } = card1;
     // const { rank: card2Rank, value: card2Suit } = card2;
-    const card1Rank = valueConvert(card1.rank);
-    const card2Rank = valueConvert(card2.rank);
+    const card1Rank = rankConvert(card1.rank);
+    const card2Rank = rankConvert(card2.rank);
     if (card1Rank === card2Rank
-      || card1Rank > 10 || card2Rank > 10 ||
-      (card1.suit === card2.suit)) {
+      || (card1Rank > 10 || card2Rank > 10)
+      || (card1.suit === card2.suit)) {
       return true;
     }
     return false;
@@ -55,15 +55,20 @@ class Player {
     const { players, current_buy_in, community_cards, minimum_raise } = gameState;
     let betting = false;
     const hole_cards = players.filter((p) => p.hole_cards.length > 0)[0].hole_cards;
-    if (hole_cards.length === 2) {
-      betting = twoCardCheck(hole_cards);
-    }
-    if (betting) {
+    if (community_cards.length === 0) {
+      if (hole_cards.length === 2) {
+        betting = twoCardCheck(hole_cards);
+      }
+      if (betting) {
+        bet(current_buy_in + (minimum_raise * 2));
+      }
+      else {
+        bet(0);
+      }
+    } else {
       bet(current_buy_in + minimum_raise);
     }
-    else {
-      bet(0);
-    }
+
   }
 
   static showdown(gameState) {
