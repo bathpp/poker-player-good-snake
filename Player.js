85dd26b4014a55b76c6718fb9b5889f54996dfd4
YHe <bathpp@gmail.com>
2023-06-08 10:10:12 -0400

    Update Player.js

--- Player.js
+++ Player.js
@@ -3,8 +3,28 @@ class Player {
     return '0.1';
   }
 
+  twoCardChecks(cards) {
+    const [card1, card2] = cards;
+    if (card1.rank === card2.rank) {
+      return true;
+    }
+    return false;
+
+  }
   static betRequest(gameState, bet) {
-    bet(0);
+    const { players, current_buy_in } = gameState;
+    let betting = false;
+    const hole_cards = players.filter((p) => p.hole_cards.length > 0)[0].hole_cards;
+    if (hole_cards.length === 2) {
+      betting = twoCardChecks(hole_cards);
+    }
+    if (betting) {
+      bet(current_buy_in);
+    }
+    else {
+      bet(0);
+    }
+
   }
 
   static showdown(gameState) {
