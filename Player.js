b561f5194fbed7c1c6dcebebdb5f92980e33d62c
yulin he <yulin.he@rushstreetinteractive.com>
2023-06-08 10:27:12 -0400

    Revert "Update Player.js"
    This reverts commit 85dd26b4014a55b76c6718fb9b5889f54996dfd4.

--- Player.js
+++ Player.js
@@ -3,28 +3,8 @@ class Player {
     return '0.1';
   }
 
-  twoCardChecks(cards) {
-    const [card1, card2] = cards;
-    if (card1.rank === card2.rank) {
-      return true;
-    }
-    return false;
-
-  }
   static betRequest(gameState, bet) {
-    const { players, current_buy_in } = gameState;
-    let betting = false;
-    const hole_cards = players.filter((p) => p.hole_cards.length > 0)[0].hole_cards;
-    if (hole_cards.length === 2) {
-      betting = twoCardChecks(hole_cards);
-    }
-    if (betting) {
-      bet(current_buy_in);
-    }
-    else {
-      bet(0);
-    }
-
+    bet(0);
   }
 
   static showdown(gameState) {
