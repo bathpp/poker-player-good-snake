72f594e4712a970887936d050790eac1b1ac5c4c
Charles Silva <silva@rushstreetinteractive.com>
2023-06-08 09:55:35 -0400

    importing axios

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+const axios = require('axios');
 
 const app = express();
 const port = parseInt((process.env.PORT || 1337), 10);
