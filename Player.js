99f69263d2bd2226d6059a8309884b704841c870
yulin he <yulin.he@rushstreetinteractive.com>
2023-06-08 10:26:24 -0400

    Revert "test"
    This reverts commit ffaec8b57cc64217efd86a13d69e12b0675c573d.

--- Player.js
+++ Player.js
@@ -3,29 +3,12 @@ class Player {
     return '0.1';
   }
 
-  twoCardCheck(cards) {
+  twoCardChecks(cards) {
     const [card1, card2] = cards;
     if (card1.rank === card2.rank) {
       return true;
     }
     return false;
-  }
-  threeCardCheck(cards) {
-
-  }
-  fourCardCheck(cards) {
-
-  }
-  fiveCardCheck(cards) {
-
-  }
-  sixCardCheck(cards) {
-
-  }
-  sevenCardCheck(cards) {
-
-  }
-  mergeCardsWithRiver(hole_cards, river) {
 
   }
   static betRequest(gameState, bet) {
@@ -33,7 +16,7 @@ class Player {
     let betting = false;
     const hole_cards = players.filter((p) => p.hole_cards.length > 0)[0].hole_cards;
     if (hole_cards.length === 2) {
-      betting = twoCardCheck(hole_cards);
+      betting = twoCardChecks(hole_cards);
     }
     if (betting) {
       bet(current_buy_in);
