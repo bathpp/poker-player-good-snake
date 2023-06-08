9d5c2d01821018937cc83f7b6d133c74cfb8125c
yulin he <yulin.he@rushstreetinteractive.com>
2023-06-08 10:53:42 -0400

    1st try

--- Player.js
+++ Player.js
@@ -3,8 +3,46 @@ class Player {
     return '0.1';
   }
 
+  static twoCardCheck(cards) {
+    const [card1, card2] = cards;
+    // const { rank: card1Rank, value: card1Suit } = card1;
+    // const { rank: card2Rank, value: card2Suit } = card2;
+    if (card1.rank === card2.rank) {
+      return true;
+    }
+    return false;
+  }
+  // threeCardCheck(cards) {
+
+  // }
+  // fourCardCheck(cards) {
+
+  // }
+  // fiveCardCheck(cards) {
+
+  // }
+  // sixCardCheck(cards) {
+
+  // }
+  // sevenCardCheck(cards) {
+
+  // }
+  // mergeCardsWithRiver(hole_cards, river) {
+
+  // }
   static betRequest(gameState, bet) {
-    bet(0);
+    const { players, current_buy_in } = gameState;
+    let betting = false;
+    const hole_cards = players.filter((p) => p.hole_cards.length > 0)[0].hole_cards;
+    if (hole_cards.length === 2) {
+      betting = twoCardCheck(hole_cards);
+    }
+    if (betting) {
+      bet(current_buy_in);
+    }
+    else {
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
